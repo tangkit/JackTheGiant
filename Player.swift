697d9c93c036359a38f3ea413d0dd8a85ce30ecf
Tang Kit <tang_kit@yahoo.com>
2016-09-18 22:34:49 +0800

    Add player

--- Player.swift
+++ Player.swift
@@ -10,6 +10,37 @@ import SpriteKit
 
 class Player: SKSpriteNode {
     
+    private var textureAtlas = SKTextureAtlas();
+    private var playerAnimation = [SKTexture]();
+    private var animatePlayerAction = SKAction();
+    
+    func initializePlayerAndAnimations (){
+
+        textureAtlas = SKTextureAtlas(named: "Player.atlas");
+        
+        for i in 2...textureAtlas.textureNames.count {
+            let name = "Player \(i)";
+            playerAnimation.append(SKTexture(imageNamed: name));
+        }
+         
+        animatePlayerAction = SKAction.animateWithTextures(playerAnimation, timePerFrame: 0.08, resize: true, restore: false);
+    }
+    
+    func animatePlayer(moveLeft: Bool){
+        if moveLeft{
+            self.xScale = -fabs(self.xScale);
+        } else {
+            self.xScale = fabs(self.xScale);
+        }
+        self.runAction(SKAction.repeatActionForever(animatePlayerAction), withKey: "Animate");
+    }
+    
+    func stopPlayerAnimation(){
+        self.removeActionForKey("Animate");
+        self.texture = SKTexture(imageNamed: "Player 1");
+        self.size = (self.texture?.size())!;
+    }
+    
     func movePlayer(moveLeft:Bool){
         
         if moveLeft {
