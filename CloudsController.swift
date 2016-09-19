7a5eeb4b325cb46f1559f5d8948991b7e09feca1
Tang Kit <tang_kit@yahoo.com>
2016-09-19 14:17:26 +0800

    Updated CloudController.swift for random clouds and physics body

--- CloudsController.swift
+++ CloudsController.swift
@@ -54,7 +54,28 @@ class CloudsController {
             darkCloud.xScale = 0.9;
             darkCloud.yScale = 0.9;
         
-            // still need to add physics body to the cloud
+            cloud1.physicsBody = SKPhysicsBody (rectangleOfSize: CGSize(width: cloud1.size.width - 6, height: cloud1.size.height - 7));
+            cloud1.physicsBody?.affectedByGravity = false;
+            cloud1.physicsBody?.restitution = 0;
+            cloud1.physicsBody?.categoryBitMask = ColliderType.Cloud;
+            cloud1.physicsBody?.collisionBitMask = ColliderType.Player;
+            
+            cloud2.physicsBody = SKPhysicsBody (rectangleOfSize: CGSize(width: cloud2.size.width - 6, height: cloud2.size.height - 7));
+            cloud2.physicsBody?.affectedByGravity = false;
+            cloud2.physicsBody?.restitution = 0;
+            cloud2.physicsBody?.categoryBitMask = ColliderType.Cloud;
+            cloud2.physicsBody?.collisionBitMask = ColliderType.Player;
+            
+            cloud3.physicsBody = SKPhysicsBody (rectangleOfSize: CGSize(width: cloud3.size.width - 6, height: cloud3.size.height - 7));
+            cloud3.physicsBody?.affectedByGravity = false;
+            cloud3.physicsBody?.restitution = 0;
+            cloud3.physicsBody?.categoryBitMask = ColliderType.Cloud;
+            cloud3.physicsBody?.collisionBitMask = ColliderType.Player;
+            
+            darkCloud.physicsBody = SKPhysicsBody (rectangleOfSize: darkCloud.size);
+            darkCloud.physicsBody?.affectedByGravity = false;
+            darkCloud.physicsBody?.categoryBitMask = ColliderType.DarkCloudAndCollectibles;
+            darkCloud.physicsBody?.collisionBitMask = ColliderType.Player;
             
             clouds.append(cloud1);
             clouds.append(cloud2);
@@ -85,9 +106,21 @@ class CloudsController {
             positionY = lastCloudPositionY;
         }
         
+        
+        var random = 0;
         for i in 0...clouds.count - 1 {
             
-            clouds[i].position = CGPoint(x: 0, y: positionY);
+            var randomX = CGFloat();
+            
+            if random == 0 {
+                randomX = randomBetweenNumbers(center + 90, secondNum: maxX);
+                random = 1;
+            } else if random == 1 {
+                randomX = randomBetweenNumbers(center - 90, secondNum: minX);
+                random = 0;
+            }
+            
+            clouds[i].position = CGPoint(x: randomX, y: positionY);
             clouds[i].zPosition = 3;
             scene.addChild(clouds[i]);
             positionY -= distanceBetweenClouds;
